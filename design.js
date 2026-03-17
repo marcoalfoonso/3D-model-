da618963d4b642964b43548ce8c7bdc33747f3a9
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-03-16 21:52:19 -0600

    the robot moves

--- design.js
+++ design.js
@@ -2,134 +2,130 @@
 
 //scene, camera, renderer
 
-const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(window.innerWidth,window.innerHeight);
+document.addEventListener("DOMContentLoaded", function(){
 
-document.body.appendChild(renderer.domElement);
+    const renderer = new THREE.WebGLRenderer();
 
-const scene = new THREE.Scene();
+    renderer.setSize(window.innerWidth*0.75,window.innerHeight*0.75);
 
-const camera = new THREE.PerspectiveCamera(
-    100,
-    window.innerWidth/window.innerHeight,
-    0.1,
-    1000
-);
+    document.body.appendChild(renderer.domElement);
 
+    const scene = new THREE.Scene();
 
-//axis helper, comera position, grid helper
-
-const axesHelper = new THREE.AxesHelper(14);
-scene.add(axesHelper);
-camera.position.set(5,3,12.5);
-
-const grid = new THREE.GridHelper(size=30, divisions=20);
-scene.add(grid);
-
-//box geometry, material, mesh, add to scene, position
-
-const geometry = new THREE.BoxGeometry(0.5,0.5,0.5);
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00,wireframe:true } );
-const cube = new THREE.Mesh( geometry, material );
-
-scene.add( cube );
+    const camera = new THREE.PerspectiveCamera(
+        100,
+        window.innerWidth/window.innerHeight,
+        0.1,
+        1000
+    );
 
 
-//adding a line1
+    //axis helper, comera position, grid helper
 
-//assessing zyz from joint 2
+    const axesHelper = new THREE.AxesHelper(14);
+    scene.add(axesHelper);
+    camera.position.set(5,3,12.5);
 
-const b1 = 3.8;
-const b2 = 2.3;
-const l1 = 4.5;
-const l2 = 8.8;
+    const grid = new THREE.GridHelper(size=30, divisions=20);
+    scene.add(grid);
 
-let q1 = -Math.PI/4;
-let q2 = Math.PI/4;
-let q3 = -Math.PI/2;
+    //box geometry, material, mesh, add to scene, position
 
-let x1 = l1 * Math.cos(q1) * Math.cos(q2);
-let y1 = b1+b2+l1*Math.sin(q2);
-let z1 = -l1*Math.sin(q1)*Math.cos(q2);
+    const geometry = new THREE.BoxGeometry(0.5,0.5,0.5);
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00,wireframe:true } );
+    const cube = new THREE.Mesh( geometry, material );
 
-let x2 = Math.cos(q1)*(l2*Math.cos(q2+q3)+l1*Math.cos(q2));
-let y2 = b1+b2+l1*Math.sin(q2)+l2*Math.sin(q2+q3);
-let z2 = -Math.sin(q1)*(l2*Math.cos(q2+q3)+l1*Math.cos(q2));
+    scene.add( cube );
 
-const base = new THREE.Vector3(0,0,0);
-const p1 = new THREE.Vector3(0,b1+b2,0);
-let p2 = new THREE.Vector3(x1,y1,z1);
-let ef = new THREE.Vector3(x2,y2,z2);
 
-let zeroF = new THREE.BufferGeometry().setFromPoints([
-    base,
-    ef 
-]);
+    //adding a line1
 
-const T01 = new THREE.BufferGeometry().setFromPoints([
-    base,
-    p1 
-]);
+    //assessing zyz from joint 2
 
-let T12 = new THREE.BufferGeometry().setFromPoints([
-    p1,
-    p2 
-]);
+    const b1 = 3.8;
+    const b2 = 2.3;
+    const l1 = 4.5;
+    const l2 = 8.8;
 
-let T23 = new THREE.BufferGeometry().setFromPoints([
-    p2,
-    ef 
-]);
+    let q1 = 0;
+    let q2 = 0;
+    let q3 = 0;
 
-const material2 = new THREE.LineBasicMaterial({ color: 0x0000ff });
-const material3 = new THREE.LineBasicMaterial({ color: 0xff0000 });
-const material4 = new THREE.LineBasicMaterial({ color: 0x00ff00 });
-const material5 = new THREE.LineBasicMaterial({ color: 0xffff00 });
+    let x1 = l1 * Math.cos(q1) * Math.cos(q2);
+    let y1 = b1+b2+l1*Math.sin(q2);
+    let z1 = -l1*Math.sin(q1)*Math.cos(q2);
 
-let line = new THREE.Line(zeroF, material2);
+    let x2 = Math.cos(q1)*(l2*Math.cos(q2+q3)+l1*Math.cos(q2));
+    let y2 = b1+b2+l1*Math.sin(q2)+l2*Math.sin(q2+q3);
+    let z2 = -Math.sin(q1)*(l2*Math.cos(q2+q3)+l1*Math.cos(q2));
 
-const line1 = new THREE.Line(T01, material3);
-let line2 = new THREE.Line(T12, material4);
-let line3 = new THREE.Line(T23, material5);
+    const base = new THREE.Vector3(0,0,0);
+    const p1 = new THREE.Vector3(0,b1+b2,0);
+    let p2 = new THREE.Vector3(x1,y1,z1);
+    let ef = new THREE.Vector3(x2,y2,z2);
 
-scene.add(line1);
-scene.add(line);
-scene.add(line2);
-scene.add(line3);
+    let zeroF = new THREE.BufferGeometry().setFromPoints([
+        base,
+        ef 
+    ]);
 
-function animate(){
-    q1 += 0.01;
-    q2 += 0.01;
-    q3 += 0.01;
+    const T01 = new THREE.BufferGeometry().setFromPoints([
+        base,
+        p1 
+    ]);
 
-    x1 = l1 * Math.cos(q1) * Math.cos(q2);
-    y1 = b1+b2+l1*Math.sin(q2);
-    z1 = -l1*Math.sin(q1)*Math.cos(q2);
-
-    x2 = Math.cos(q1)*(l2*Math.cos(q2+q3)+l1*Math.cos(q2));
-    y2 = b1+b2+l1*Math.sin(q2)+l2*Math.sin(q2+q3);
-    z2 = -Math.sin(q1)*(l2*Math.cos(q2+q3)+l1*Math.cos(q2));
-
-    p2 = new THREE.Vector3(x1,y1,z1);
-    ef = new THREE.Vector3(x2,y2,z2);
-
-    T12 = new THREE.BufferGeometry().setFromPoints([
+    let T12 = new THREE.BufferGeometry().setFromPoints([
         p1,
         p2 
     ]);
 
-    T23 = new THREE.BufferGeometry().setFromPoints([
+    let T23 = new THREE.BufferGeometry().setFromPoints([
         p2,
         ef 
     ]);
 
-    line = new THREE.Line(zeroF, material2);
-    line2 = new THREE.Line(T12, material4);
-    line3 = new THREE.Line(T23, material5);
-    
-    renderer.render(scene,camera);
-}
- 
+    const material2 = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    const material3 = new THREE.LineBasicMaterial({ color: 0xff0000 });
+    const material4 = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+    const material5 = new THREE.LineBasicMaterial({ color: 0xffff00 });
 
-renderer.setAnimationLoop(animate);
+    let line = new THREE.Line(zeroF, material2);
+
+    const line1 = new THREE.Line(T01, material3);
+    let line2 = new THREE.Line(T12, material4);
+    let line3 = new THREE.Line(T23, material5);
+
+    scene.add(line1);
+    scene.add(line);
+    scene.add(line2);
+    scene.add(line3);
+
+    function animate(){
+        q1 += 0.01;
+        q2 += 0.01;
+        q3 += 0.01;
+
+        x1 = l1 * Math.cos(q1) * Math.cos(q2);
+        y1 = b1+b2+l1*Math.sin(q2);
+        z1 = -l1*Math.sin(q1)*Math.cos(q2);
+
+        x2 = Math.cos(q1)*(l2*Math.cos(q2+q3)+l1*Math.cos(q2));
+        y2 = b1+b2+l1*Math.sin(q2)+l2*Math.sin(q2+q3);
+        z2 = -Math.sin(q1)*(l2*Math.cos(q2+q3)+l1*Math.cos(q2));
+
+        p2.set(x1,y1,z1);
+        ef.set(x2,y2,z2);
+
+        line.geometry.setFromPoints([base,ef]);
+        line2.geometry.setFromPoints([p1,p2]);
+        line3.geometry.setFromPoints([p2,ef]);
+
+        renderer.render(scene,camera);
+    }
+    
+
+
+    renderer.setAnimationLoop(animate);
+
+});
